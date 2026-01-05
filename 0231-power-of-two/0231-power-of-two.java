b70d0d153848c856cd8ee943439f4d30ae6657f6
Suman Dey <147734727+sumandey09@users.noreply.github.com>
2026-01-05 19:08:10 +0530

    Time: 1 ms (95.55%), Space: 42.6 MB (54.41%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -4,13 +4,11 @@ class Solution {
             return false;
         }
         while(n!=1){
-        if(n%2!=0){
-            
-            return false;
-        }else{
-            n=n/2;
-        }
-
+            if(n%2!=0){
+                return false;
+            }else{
+                n=n/2;
+            }
         }
         return true;
     }
