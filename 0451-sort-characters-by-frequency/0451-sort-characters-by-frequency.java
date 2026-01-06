b8e4339f0f0a18be5d32dcd99be7633d2a070a29
Suman Dey <147734727+sumandey09@users.noreply.github.com>
2026-01-06 09:38:19 +0530

    Time: 5 ms (96.59%), Space: 46.4 MB (77.99%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,36 @@
+class Solution {
+    class Pair{
+        char ch;
+        int count;
+        Pair(char ch,int count){
+            this.ch=ch;
+            this.count=count;
+        }
+    }
+    public String frequencySort(String s) {
+        int arr[]=new int[125];
+        for(char ch:s.toCharArray()){
+            arr[ch]++;
+        }
+        LinkedList<Pair> freq_list=new LinkedList<>();
+        for(int i=48;i<=122;i++){
+            if(arr[i]>0){
+                freq_list.add(new Pair((char )i,arr[i]));
+            }
+        }
+        Collections.sort(freq_list,new Comparator<Pair>(){
+            public int compare(Pair a,Pair b){
+                return b.count-a.count;
+
+            }
+        });
+        StringBuilder sb=new StringBuilder();
+        for(Pair p:freq_list){
+            int count=p.count;
+            while(count-->0){
+                sb.append(p.ch);
+            }
+        }
+        return sb.toString();
+    }
+}
